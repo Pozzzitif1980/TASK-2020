5bdff56353e263e5ad0085f4273d410148a96511
Pozzzitif1980 <92913891+Pozzzitif1980@users.noreply.github.com>
2021-11-18 11:22:23 +0200

    Update users.php

--- html/users.php
+++ html/users.php
@@ -7,18 +7,15 @@
    $sql = "SELECT * FROM `users`";
    $result = $conn->query($sql); 
    
-  
-   
    while ($row = $result->fetch_assoc())
    {
-       echo "" . $row["Id"]. "<br>";
+       
        echo "Username: " . $row["Username"]. "<br>";
        echo "Email: " . $row["Email"]. "<br>";
        echo "Password: " . $row["Password"]. "<br>";
        echo "Role: " . $row["Role"]. "<br>";
        echo "Created: " . $row["Created"]. "<br>";
-       echo "Updated: " . $row["Updated"]. "<br>";
+       echo "Updated: " . $row["Updated"]. "<p>";
        
    }
 ?>
-
