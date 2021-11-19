65dc3f4cd7f9d21450201f914cf9b1262787c152
Pozzzitif1980 <92913891+Pozzzitif1980@users.noreply.github.com>
2021-11-19 16:08:28 +0200

    Update users.php

--- html/users.php
+++ html/users.php
@@ -1,21 +1,58 @@
 <?php
-   $hostname = 'localhost';
-   $username = 'root';
-   $passwordname = '';
-   $basename = 'test';
-   $conn = new mysqli($hostname, $username, $passwordname, $basename) or die       ('Невозможно открыть базу');
-   $sql = "SELECT * FROM `users`";
-   $result = $conn->query($sql); 
-   
-   while ($row = $result->fetch_assoc())
-   {
-       
-       echo "Username: " . $row["Username"]. "<br>";
-       echo "Email: " . $row["Email"]. "<br>";
-       echo "Password: " . $row["Password"]. "<br>";
-       echo "Role: " . $row["Role"]. "<br>";
-       echo "Created: " . $row["Created"]. "<br>";
-       echo "Updated: " . $row["Updated"]. "<p>";
-       
-   }
+require_once 'connect.php';
+
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>USERS</title>
+</head>
+<style>
+    th, td{
+        padding: 10px;
+    }
+    th {
+        background: #606060;
+        color: lemonchiffon;
+    }
+    td {
+        background: beige;
+    }
+</style>
+<body>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Role</th>
+            <th>Created</th>
+            <th>Update</th>
+        </tr>
+        <?php
+        $users = mysqli_query($connect, "SELECT * FROM `users`");
+        $users = mysqli_fetch_all($users);
+        print_r($user);
+        foreach ($users as $user) {
+            ?>
+                <tr>
+                    <td><?= $user[0] ?></td>
+                    <td><?= $user[1] ?></td>
+                    <td><?= $user[2] ?></td>
+                    <td><?= $user[3] ?></td>
+                    <td><?= $user[4] ?></td>
+                    <td><?= $user[5] ?></td>
+                    <td><?= $user[6] ?></td>
+                </tr>
+
+
+           <?php
+        }
+        ?>
+
+    </table>
+    </body>
+</html>
